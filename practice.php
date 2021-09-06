f40f8ac6e1664507d0d598aaf79507ed0e459f9d
stente3 <stentem0103@gmail.com>
2021-09-05 20:51:33 -0500

    the project was added

---
+++ practice.php
@@ -0,0 +1,20 @@
+<php? ?>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>HOla</title>
+    </head>
+
+    <body>
+        <p>Hola mi nombre es eppepe</p>
+        <p>Hola mi nombre es eppepe</p>
+        <p>Hola mi nombre es eppepe</p>
+        <p>Hola mi nombre es eppepe</p>
+        <p>Hola mi nombre es eppepe</p>
+    </body>
+
+    </html>
