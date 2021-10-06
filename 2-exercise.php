26d09d89357865156464309a68042a1d671410c3
stente3 <stentem0103@gmail.com>
2021-10-06 08:03:49 -0500

    New exercises was added

---
+++ 2-exercise.php
@@ -0,0 +1,21 @@
+<!-- PHP echo statement can be used to print the string, multi-line strings, escaping characters, variable, array, etc. 
+... echo is a statement, which is used to display the output. echo can be used with or without parentheses: echo(), and echo. 
+echo does not return any value. -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./favicon.svg" type="image/x-icon">
+    <title>2-exercise</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
+<?php
+echo "Hola mundo";
