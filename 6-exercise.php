fb876ddbaffe3f8a569fc1453c2e366011f36a9a
stente3 <stentem0103@gmail.com>
2021-10-07 07:56:20 -0500

    New exercises was added

---
+++ 6-exercise.php
@@ -0,0 +1,31 @@
+<!-- Comparison operators are used in conditional expressions to determine which block of code executes, thus controlling the program flow.
+    Comparison operators compare two values in an expression that resolves to a value of true or false -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./favicon.svg" type="image/x-icon">
+    <title>Comparison operators</title>
+</head>
+
+<body>
+    <?php
+    $a = 10;
+    $b = 20;
+    $c = 10;
+
+    echo $a === $b;
+    echo $a === $c;
+    /* echo $b === $c;
+    echo $a !== $b;
+    echo $a !== $c;
+    echo $a < $b;
+    echo $a > $b;
+    echo $a < $c; */
+    ?>
+</body>
+
+</html>
